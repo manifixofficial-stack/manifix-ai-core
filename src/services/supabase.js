c297f2aafa40a8ac7108e77a58e60a1fa0e38e67
manifixofficial-stack <manifixofficial@gmail.com>
2026-02-27 21:52:13 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,10 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase environment variables are missing");
-}
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
