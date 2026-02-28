daade12827b1c895530b8808315c111b53bf716a
manifixofficial-stack <manifixofficial@gmail.com>
2026-02-28 22:34:40 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // src/components/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useApp } from "../context/AppContext";
+import { useApp } from "./context/AppContext";
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useApp();
