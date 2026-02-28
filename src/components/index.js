3648b4d560de4d4290851d64a158d6d3f71c07b9
manifixofficial-stack <manifixofficial@gmail.com>
2026-02-28 11:08:25 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,36 @@
+// src/components/index.js
+
+// Layout
+export { default as MainLayout } from "./Layout/MainLayout";
+export { default as MobileLayout } from "./Layout/MobileLayout";
+export { default as TopBar } from "./Layout/TopBar";
+export { default as BottomNav } from "./Layout/BottomNav";
+
+// Chat
+export { default as ChatBox } from "./ChatBox";
+export { default as ChatInput } from "./ChatInput";
+export { default as ChatMessage } from "./ChatMessage";
+
+// Magic16
+export { default as Magic16Controls } from "./Magic16Controls";
+export { default as Magic16Timer } from "./Magic16Timer";
+
+// Vibe
+export { default as ManifestList } from "./ManifestList";
+export { default as ProfileCard } from "./ProfileCard";
+export { default as VibeCreate } from "./VibeCreate";
+export { default as VibeFeed } from "./VibeFeed";
+export { default as VibeItem } from "./VibeItem";
+export { default as VibePrivacyToggle } from "./VibePrivacyToggle";
+export { default as VibeStoryPlayer } from "./VibeStoryPlayer";
+
+// Camera / Vision
+export { default as CameraView } from "./Camera/CameraView";
+export { default as CameraStats } from "./Camera/CameraStats";
+export { default as EmotionOverlay } from "./Camera/EmotionOverlay";
+export { default as VisionStats } from "./Camera/VisionStats";
+
+// Utilities / Standalone
+export { default as Button } from "./Button";
+export { default as Loader } from "./Loader";
+export { default as Modal } from "./Modal";
