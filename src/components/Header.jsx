95c1642cf4a9c4c9220780234c6e55805571ec51
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-01 18:18:51 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Logo from "../assets/logo.png";
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <header className="gpt-header">
+      <img src={Logo} alt="ManifiX Logo" className="gpt-logo" />
+      <h1>ManifiX</h1>
+
+      {/* Magic16 "+" button */}
+      <button
+        className="magic16-add"
+        title="New Magic16 Connection"
+        onClick={() => navigate("/magic16")}
+      >
+        ➕
+      </button>
+    </header>
+  );
+}
