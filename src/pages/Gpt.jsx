fb76024bae52b23411a5b28082209b8e7794600b
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-03 19:59:14 +0530

    Update Gpt.jsx

--- src/pages/Gpt.jsx
+++ src/pages/Gpt.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import Confetti from "react-confetti";
 import "../styles/Gpt.css";
 import backgroundPurple from "../assets/backgrounds/purple-vibe.jpg";
 import Header from "../components/Header";
