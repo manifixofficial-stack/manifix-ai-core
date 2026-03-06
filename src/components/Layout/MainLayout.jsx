a88494420c149bfd77ea08ddeff09a8c32eaf864
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-07 02:07:37 +0530

    Update MainLayout.jsx

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 import "../styles/MainLayout.css";
 
 export default function MainLayout() {
