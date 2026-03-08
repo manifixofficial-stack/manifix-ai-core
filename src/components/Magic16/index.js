e3b069a84462424bd348e8461d653e5df0e64bcd
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-09 00:48:28 +0530

    Update index.js

--- src/components/Magic16/index.js
+++ src/components/Magic16/index.js
@@ -1,2 +1,3 @@
 export { default as Magic16Controls } from "./Magic16Controls";
 export { default as Magic16Timer } from "./Magic16Timer";
+export { default as PostureOverlay} from "./PostureOverlay";
