1b1e7dc78fe1e8a803438dbf1b6a1b7bcab8e88c
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-09 01:23:11 +0530

    Update MainLayout.jsx

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -53,15 +53,7 @@ export default function MainLayout() {
         </svg>
       ),
     },
-    {
-      name: "Contact",
-      path: "/app/contact",
-      icon: (
-        <svg width="20" height="20" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
-        </svg>
-      ),
-    },
+   
   ];
 
   return (
