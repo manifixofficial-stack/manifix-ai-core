066436050373de403a6d990d8671b4d78251b97b
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-10 12:26:18 +0530

    Update index.js

--- src/components/Magic16/index.js
+++ src/components/Magic16/index.js
@@ -1,3 +1,10 @@
 export { default as Magic16Controls } from "./Magic16Controls";
 export { default as Magic16Timer } from "./Magic16Timer";
-export { default as PostureOverlay} from "./PostureOverlay";
+export { default as PostureOverlay } from "./PostureOverlay";
+
+export { default as Magic16Step } from "./Magic16Step";
+export { default as Magic16Progress } from "./Magic16Progress";
+export { default as Magic16Score } from "./Magic16Score";
+export { default as Magic16Complete } from "./Magic16Complete";
+export { default as Magic16Share } from "./Magic16Share";
+export { default as BreathingCircle } from "./BreathingCircle";
