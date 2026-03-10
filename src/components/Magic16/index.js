e1fbf924f143f26112b2c4c94e8b97595e21b3f4
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-10 13:08:38 +0530

    Update index.js

--- src/components/Magic16/index.js
+++ src/components/Magic16/index.js
@@ -1,10 +1,10 @@
-export { default as Magic16Controls } from "./Magic16Controls";
-export { default as Magic16Timer } from "./Magic16Timer";
-export { default as PostureOverlay } from "./PostureOverlay";
+export { default as Magic16Controls } from "./Magic16Controls.jsx";
+export { default as Magic16Timer } from "./Magic16Timer.jsx";
+export { default as PostureOverlay } from "./PostureOverlay.jsx";
 
-export { default as Magic16Step } from "./Magic16Step";
-export { default as Magic16Progress } from "./Magic16Progress";
-export { default as Magic16Score } from "./Magic16Score";
-export { default as Magic16Complete } from "./Magic16Complete";
-export { default as Magic16Share } from "./Magic16Share";
-export { default as BreathingCircle } from "./BreathingCircle";
+export { default as Magic16Step } from "./Magic16Step.jsx";
+export { default as Magic16Progress } from "./Magic16Progress.jsx";
+export { default as Magic16Score } from "./Magic16Score.jsx";
+export { default as Magic16Complete } from "./Magic16Complete.jsx";
+export { default as Magic16Share } from "./Magic16Share.jsx";
+export { default as BreathingCircle } from "./BreathingCircle.jsx";
