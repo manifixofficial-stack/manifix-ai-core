f867e1f30b23816666b72d7c15a14ffc68e09354
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-13 00:33:26 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,33 +1,35 @@
-// Layout
+// src/components/index.js
+
+// ================= Layout =================
 export { default as MainLayout } from "./Layout/MainLayout";
 export { default as MobileLayout } from "./Layout/MobileLayout";
 export { default as TopBar } from "./Layout/TopBar";
 export { default as BottomNav } from "./Layout/BottomNav";
 
-// Chat (inside Chat folder)
+// ================= Chat =================
 export { default as ChatBox } from "./Chat/ChatBox";
 export { default as ChatInput } from "./Chat/ChatInput";
 export { default as ChatMessage } from "./Chat/ChatMessage";
-// Magic16
+
+// ================= Magic16 =================
 export { default as Magic16Controls } from "./Magic16/Magic16Controls";
 export { default as Magic16Timer } from "./Magic16/Magic16Timer";
-export { default as PostureOverlay } from "./Magic16/PostureOverlay";
-
 export { default as Magic16Step } from "./Magic16/Magic16Step";
 export { default as Magic16Progress } from "./Magic16/Magic16Progress";
 export { default as Magic16Score } from "./Magic16/Magic16Score";
 export { default as Magic16Complete } from "./Magic16/Magic16Complete";
 export { default as Magic16Share } from "./Magic16/Magic16Share";
+export { default as PostureOverlay } from "./Magic16/PostureOverlay";
 export { default as BreathingCircle } from "./Magic16/BreathingCircle";
 
-
-// Vibe
+// ================= Profile =================
 export { default as ProfileCard } from "./Profile/ProfileCard";
 
-// Camera / Vision
+// ================= Vision / Camera =================
 export { default as CameraView } from "./Camera/CameraView";
 export { default as CameraStats } from "./Camera/CameraStats";
 export { default as EmotionOverlay } from "./Camera/EmotionOverlay";
 export { default as VisionStats } from "./Camera/VisionStats";
-export { default as Modal } from "./Modal";
 
+// ================= UI =================
+export { default as Modal } from "./Modal";
