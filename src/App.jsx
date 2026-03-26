0deec639d1bf0edd4bef4a16f59a6efc4b59b60d
manifixofficial-stack <manifixofficial@gmail.com>
2026-03-26 14:44:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-// src/App.jsx
 import React from "react";
 import AppRouter from "./AppRouter";
+import CookieBanner from "./components/CookieBanner";
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <CookieBanner /> {/* ✅ ADD THIS */}
+    </>
+  );
 }
