75555e29b60942933bd1f9dbc1ab6259e7042442
manifixofficial-stack <manifixofficial@gmail.com>
2026-04-16 23:38:19 +0530

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -30,7 +30,6 @@ import Magic16 from "./pages/Magic16";
 import Feedback from "./pages/Feedback";
 import Billing from "./pages/Billing";
 import Settings from "./pages/Settings";
-import Session from "./pages/Session";
 import Result from "./pages/Result";
 
 /* ---------------- Not Found ---------------- */
@@ -108,7 +107,6 @@ export default function AppRouter() {
 
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="gpt" element={<Gpt />} />
-          <Route path="session" element={<Session />} />
           <Route path="result" element={<Result />} />
           <Route path="magic16" element={<Magic16 />} />
           <Route path="feedback" element={<Feedback />} />
