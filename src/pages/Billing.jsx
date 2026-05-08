9dfabec02269fcfcadf98d2c820a567fe2b7e5cc
manifixofficial-stack <manifixofficial@gmail.com>
2026-05-08 14:35:33 +0530

    Update Billing.jsx

--- src/pages/Billing.jsx
+++ src/pages/Billing.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/Billing666.css";
+import "../styles/Billing555.css";
 import authService from "../services/auth.service";
 import logo from "../assets/logo.png";
 
