8dc79e923943431009913460272a727cf95bfaa1
manifixofficial-stack <manifixofficial@gmail.com>
2026-05-14 18:13:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import React, { useEffect } from "react";
+import AppRouter from "./AppRouter";
 
 export default function App() {
 
   /* Notification Permission */
   useEffect(() => {
-    if ("Notification" in window) {
+    if ("Notification" in window") {
       Notification.requestPermission();
     }
   }, []);
 
-  return (
-    <div
-      style={{
-        background: "#000",
-        color: "gold",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "40px",
-        fontWeight: "bold",
-        fontFamily: "Poppins, sans-serif",
-      }}
-    >
-      ManifiX AI
-    </div>
-  );
+  return <AppRouter />;
 }
