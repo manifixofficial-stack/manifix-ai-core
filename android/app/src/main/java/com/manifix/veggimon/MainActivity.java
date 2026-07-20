6bb64771ae70673671f54e0d934ab64846c80dd8
manifixofficial-stack <manifixofficial@gmail.com>
2026-07-20 11:29:50 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/manifix/veggimon/MainActivity.java
+++ android/app/src/main/java/com/manifix/veggimon/MainActivity.java
@@ -1,5 +1,13 @@
 package com.manifix.veggimon;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.manifix.veggimon.VeggieGoARPlugin; // 1. Import your new vegetable AR plugin
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(VeggieGoARPlugin.class); // 2. Register the plugin here
+        super.onCreate(savedInstanceState);
+    }
+}
